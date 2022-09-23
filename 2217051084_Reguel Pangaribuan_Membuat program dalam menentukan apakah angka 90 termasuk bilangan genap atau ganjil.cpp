6804c455798087f28bfb2e5db97502cc50a9c81d
Reguel Andreas Agustinus Pangaribuan <112568303+reguelpangrib29@users.noreply.github.com>
2022-09-23 21:13:21 +0700

    Add files via upload

---
+++ 2217051084_Reguel Pangaribuan_Membuat program dalam menentukan apakah angka 90 termasuk bilangan genap atau ganjil.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //contoh if else program menentukan apakah angka 90 termasuk bilangan genap atau ganjil
+    int bilangan;
+    cin>>bilangan;
+    if(bilangan%2==0&&bilangan>0){
+     	cout<<"bilangan genap"<<endl;
+     	cout<<"bilangan positif";
+    }
+    else if(bilangan%2==1&&bilangan>0){
+    	cout<<"bilangan ganjil"<<endl;
+    	
+    }
+    
+    
+    
+        return 0;
+         
+    }
+    
+
+
+
+
+	
+
+     
+     
